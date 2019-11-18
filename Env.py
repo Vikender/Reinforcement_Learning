d69281e67cc1ccba4a32db38475128defd862ac0
Vikender <vikcysingh54@gmail.com>
2019-11-19 00:40:00 +0530

    Add files via upload

--- Env.py
+++ Env.py
@@ -21,7 +21,7 @@ class CabDriver():
             for j in range(0,m):
                 if i!=j:
                     self.action_space.append((i,j))        
-        self.state_space = [(i,j,k) for i in range(1,m+1) for j in range(0,t) for k in range(0,d)]
+        self.state_space = [(i,j,k) for i in range(0,m) for j in range(0,t) for k in range(0,d)]
         self.state_init = self.state_space[np.random.choice(np.arange(0,len(self.state_space)))]
 
         # Start the first round
@@ -39,11 +39,11 @@ class CabDriver():
     # Use this function if you are using architecture-2 
     def state_encod_arch2(self, state, action):
     #     """convert the (state-action) into a vector so that it can be fed to the NN. This method converts a given state-action pair into a vector format. Hint: The vector is of size m + t + d + m + m."""
-        state_vec=np.zeros(m+t+d+m+m)
-        state_vec[m+state[1]]=state[1]
-        state_vec[m+t+state[2]]=state[2]
-        state_vec[m+t+d+action[0]]=action[0]
-        state_vec[m+t+d+m+action[1]]=action[1]
+        state_encod=np.zeros(m+t+d+m+m)
+        state_encod[m+int(state[1])]=state[1]
+        state_encod[m+t+int(state[2])]=state[2]
+        state_encod[m+t+d+int(action[0])]=action[0]
+        state_encod[m+t+d+m+int(action[1])]=action[1]
           
         return state_encod
 
@@ -69,10 +69,10 @@ class CabDriver():
             requests =15
 
         possible_actions_index = random.sample(range(1, (m-1)*m +1), requests) # (0,0) is not considered as customer request
-        actions = [self.action_space[i] for i in possible_actions_idx]
+        actions = [self.action_space[i] for i in possible_actions_index]
 
         
-        actions.append([0,0])
+        actions.append((0,0))
 
         return possible_actions_index,actions   
 
@@ -80,14 +80,22 @@ class CabDriver():
 
     def reward_func(self, state, action, Time_matrix):
         """Takes in state, action and Time-matrix and returns the reward"""
+        curr_state=int(state[0])
+        start_state=int(action[0])
+        end_state=int(action[1])
+        time=int(state[1])
+        day=int(state[2])
         if (action[0]==0) & (action[1]==0):
             reward=-C
         else:
             if(state[0]!=action[0]):
-                time_currLoc_to_start=Time_matrix[state[0]][action[0]][state[1]][state[2]]
+                time_currLoc_to_start=Time_matrix[curr_state][start_state][time][day]
             else:
                 time_currLoc_to_start=0
-            time_state_to_end=Time_matrix[action[0]][action[1]][int(state[1]+time_currLoc_to_start)][state[2]]
+            if time+time_currLoc_to_start > 23:
+                time_state_to_end=Time_matrix[start_state][end_state][int(time+time_currLoc_to_start-24)][day]
+            else:
+                time_state_to_end=Time_matrix[start_state][end_state][int(time+time_currLoc_to_start)][day]
             reward= (R*time_state_to_end)-(C*(time_currLoc_to_start+time_state_to_end))
         return reward
 
@@ -96,15 +104,23 @@ class CabDriver():
 
     def next_state_func(self, state, action, Time_matrix):
         """Takes state and action as input and returns next state"""
-        if (action[0]==0) & (action[1]==0):
-            next_state=(state[0],Time_matrix[action[0]][action[0]][state[1]][state[2]],state[2])
+        curr_state=int(state[0])
+        start_state=int(action[0])
+        end_state=int(action[1])
+        time=int(state[1])
+        day=int(state[2])
+        if (start_state==0) & (end_state==0):
+            next_state=(curr_state,Time_matrix[curr_state][curr_state][time][day],day)
         else:
             if(state[0]!=action[0]):
-                time_currLoc_to_start=Time_matrix[state[0]][action[0]][state[1]][state[2]]
+                time_currLoc_to_start=Time_matrix[curr_state][start_state][time][day]
             else:
                 time_currLoc_to_start=0
-            time_state_to_end=Time_matrix[action[0]][action[1]][int(state[1]+time_currLoc_to_start)][state[2]]
-            next_state=(action[1],time_state_to_end,state[2])
+            if time+time_currLoc_to_start > 23:
+                time_state_to_end=Time_matrix[start_state][end_state][int(time+time_currLoc_to_start-24)][day]
+            else:
+                time_state_to_end=Time_matrix[start_state][end_state][int(time+time_currLoc_to_start)][day]
+            next_state=(end_state,time_state_to_end,state[2])
         
         return next_state
 
